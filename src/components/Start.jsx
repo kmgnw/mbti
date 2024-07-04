1217e395b1e3d7ce57cfb006a1786a34819915a9
kmgnw <wathcpropropropro@gmail.com>
2024-07-04 19:18:10 +0900

    mid-commit

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,20 +1,49 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import bannerImage from '../image/짱구배너.png';
+import bannerImage from '../image/오수형.png';
+import bannerImage1 from '../image/훈이.png';
 
 function Start() {
   const navigate = useNavigate();
 
   return (
     <div className="content" id="start">
-      <p>나와 닮은 짱구 캐릭터 테스트</p>
-      <img src={bannerImage} alt="짱구단체" className="img1" />
-      <p id="maker">제작자: 이나경</p>
+      <p style={{color: 'blue'}}>나와 닮은 짱구 캐릭터 테스트</p>
+      <div style={{
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'center',
+        gap: '20px'
+      }}>
+
+      <div>
+      <img src={bannerImage} alt="킹오수" className="img1"
+      style={{
+        margin: 0,
+        width: '30vw'
+      }}
+      />
+      </div>
+
+      <div>
+      <img src={bannerImage1} alt="킹오수" 
+      style={{
+        margin: 0,
+        width: '30vw'
+      }} className="img1" />
+      </div>
+
+      </div>
+
+      <p id="maker">제작자: 김건우,,ㅎㅎㅎ</p>
       <p id="require">
-        ※단순히 재미로 제작한 것이며, 오수나 훈이가 나와도 화내지 말아주세요!
+        ※안심하세요! 오수나 훈이가 나올 확률이 높습니다!
       </p>
-      <button className="btn btn-warning" onClick={() => navigate('/question')}>
-        떡잎마을로 출발~
+      <button className="btn btn-warning" style={{
+        backgroundColor: 'blue',
+        color: 'white'
+      }} onClick={() => navigate('/question')}>
+        오수나 훈이 되러 출발하기
       </button>
     </div>
   );
